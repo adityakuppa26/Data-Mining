e19fb9abee93a732e1b8291b1f21565e8347ca3c
Aditya Kuppa <adityakuppa27@gmail.com>
2018-10-08 22:32:06 +0530

    Create naive_bayes.r

---
+++ naive_bayes.r
@@ -0,0 +1,21 @@
+library(e1071)
+
+library(rjson)
+
+#Load Data
+
+
+input.dat <- fromJSON(file = "credit-g.json")
+
+
+#Train the Model
+
+model <- naiveBayes(BUYS_COMPUTER ~ AGE + INCOME + STUDENT +
+
+CREDIT_RATING, data = input.dat)
+
+#Predict using that model
+
+input.dat[1,]
+
+predict(model, input.dat[1,-5])
