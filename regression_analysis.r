6d9636c14bf00e1a2887798e4d3875c87cb6445f
Aditya Kuppa <adityakuppa27@gmail.com>
2018-11-30 22:59:12 +0530

    Update regression_analysis.r

--- regression_analysis.r
+++ regression_analysis.r
@@ -1,12 +1,23 @@
-# data points
+ # data points
+
+
 
 w <- c(23,56,57,60,67,68,70)
+
 h <- c(156,158,161,165,169,175,180)
+
+relation <- lm(w~h)
 
 # plot the curve
 
-plot(h,w,col="blue",main="h vs w",abline(lm(h~w)),xlab="weight",ylab="height")
+
+
+plot(h,w,col="blue",main="h vs w",abline(lm(relation)),xlab="weight",ylab="height")
+
+
 
 # exit
+
+
 
 dev.off()
